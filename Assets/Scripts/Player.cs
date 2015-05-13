b1d9f6b950255fc30e4a35a9b8b8b6ae096a271d
Viamos <victor.amoros@balmes.epiedu.cat>
2015-05-13 17:09:39 +0200

    Victor
    victor_agarra

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -126,7 +126,7 @@ public class Player : MonoBehaviour {
 	void OnCollisionEnter2D (Collision2D other)
 	{
 		Debug.Log (other.transform.name);
-		if (other.gameObject.tag == "floor" || other.gameObject.tag == "plat_peke" || other.gameObject.name == "prefPlat_med")
+		if (other.gameObject.tag == "floor" || other.gameObject.tag == "plat_peke" || other.gameObject.name == "prefPlat_med" || other.gameObject.tag=="Boss")
 		{
 			grounded = true;
 		}
