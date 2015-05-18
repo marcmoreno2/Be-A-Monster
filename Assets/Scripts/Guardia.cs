0ee2d7b9b7a5bf7c192c1fa03b02519946ca083c
gyoza14 <sergi.sanchez@balmes.epieadu.cat>
2015-05-18 22:05:01 +0100

    Sergi
    wefwf

--- Assets/Scripts/Guardia.cs
+++ Assets/Scripts/Guardia.cs
@@ -3,13 +3,34 @@ using System.Collections;
 
 public class Guardia : MonoBehaviour {
 
+	private Animator ani;
+	public float vel=-3;
+	public Transform sightStart, sightEnd;
+	public bool alerta = false;
 	// Use this for initialization
 	void Start () {
-	
+		ani = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		RayCasting ();
+		Behaviours ();
+		transform.Translate (vel*Time.deltaTime,0.0f,0.0f);
+	}
+
+	void RayCasting()
+	{
+		Debug.DrawLine (sightStart.position, sightEnd.position, Color.green);
+		alerta = Physics2D.Linecast (sightStart.position, sightEnd.position, 1 << LayerMask.NameToLayer("Player"));
+		
+		
+		
+	}
+
+	void Behaviours()
+	{
+
+
 	}
 }
