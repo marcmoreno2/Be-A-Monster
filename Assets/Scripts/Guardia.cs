3ad8976291d8f30ea4a9a7d8f03303866b56de7f
marcmoreno2 <marc.moreno@balmes.epiaedu.cat>
2015-05-19 02:21:09 +0200

    drops

--- Assets/Scripts/Guardia.cs
+++ Assets/Scripts/Guardia.cs
@@ -5,12 +5,13 @@ public class Guardia : MonoBehaviour {
 
 	private Animator ani;
 	public float vel=-3;
+	public GameObject PocionS,Hamburguesa,Pollo;
 	public Transform sightStart, sightEnd;
 	public bool alerta = false;
 	public bool ataque = false;
-	public float velfin=-3;
+	public float velfin = -3, vida = 200, fuerzaDrop;
 	public GameObject Atak;
-	public float vel_ataq=0;
+	public float vel_ataq = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -60,6 +61,27 @@ public class Guardia : MonoBehaviour {
 			ani.SetBool("ataque",false);
 
 		}
+		if (vida <= 0f) {
+
+			float rand = Random.Range(0.0f, 100.0f);
+			if(rand <=25)
+			{
+				GameObject p = Instantiate(PocionS, this.transform.position, this.transform.rotation) as GameObject;
+				p.GetComponent<Rigidbody2D>().AddForce(new Vector2(50f,fuerzaDrop));
+			}
+			else if(rand <=50)
+			{
+				GameObject p = Instantiate(Hamburguesa, this.transform.position, this.transform.rotation) as GameObject;
+				p.GetComponent<Rigidbody2D>().AddForce(new Vector2(50f,fuerzaDrop));
+			}
+			else if(rand <=75)
+			{
+				GameObject p = Instantiate(Pollo, this.transform.position, this.transform.rotation) as GameObject;
+				p.GetComponent<Rigidbody2D>().AddForce(new Vector2(50f,fuerzaDrop));
+			}
+
+			Destroy (this.gameObject);
+		}
 
 	}
 
@@ -73,6 +95,8 @@ public class Guardia : MonoBehaviour {
 			transform.localScale = aux;
 			
 		}
+		if (other.tag == "Arma player")
+			vida -= SystemVar.SystemVar.playerAtack;
 	}
 
 }
