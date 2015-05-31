5c6a88a98b929ba09c3371c5ea82fbeb734ed652
gyoza14 <sergi.sanchez@balmes.epieadu.cat>
2015-05-31 18:39:35 +0100

    Caca Paused
    fefef

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+	// Use this for initialization
+
+
+
+
+	public bool isPaused;
+	public GameObject pauseMenuCanvas;
+
+
+	public void Resume()
+	{
+		isPaused = false;
+	}
+
+	public void Menu()
+	{
+		Application.LoadLevel ("Main_Menu");
+	}
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isPaused) 
+		{
+			pauseMenuCanvas.SetActive (true);
+			Time.timeScale=0f;
+		} 
+		else 
+		{
+			pauseMenuCanvas.SetActive(false);
+			Time.timeScale =1f;
+		}
+		if (Input.GetKeyDown (KeyCode.Escape)) 
+		{
+			isPaused=!isPaused;
+		}
+	}
+}
